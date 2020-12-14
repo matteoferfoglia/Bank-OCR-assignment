e2bb077bd38d012193978e628bd0771efadd0d8f
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2020-12-14 19:51:01 +0100

    Class Digit has been deleted.

--- src/main/java/dssc/assignment/bank/Cell.java
+++ src/main/java/dssc/assignment/bank/Cell.java
@@ -2,11 +2,8 @@ package dssc.assignment.bank;
 
 public class Cell {
 
-    private static final String ZERO_CELL =
-                    " _ " +
-                    "| |" +
-                    "|_|";
     private final String cellAsText;
+    private char cellAsChar;
 
     public Cell(String cellAsText) {
         this.cellAsText = cellAsText;
@@ -14,7 +11,6 @@ public class Cell {
 
     @Override
     public String toString() {
-        //return ZERO_CELL.equals(cellAsText) ? "0": "1";
         String s;
         switch (cellAsText){
             case ZERO_CELL:
@@ -26,52 +22,47 @@ public class Cell {
         }
         return s;
     }
-}
 
-class Digit {
 
-    private static final String ZERO_CELL =
+
+    public static final String ZERO_CELL =
             " _ " +
             "| |" +
             "|_|";
-    private static final String ONE_CELL =
+    public static final String ONE_CELL =
             "   " +
             "  |" +
             "  |";
-    private static final String TWO_CELL =
+    public static final String TWO_CELL =
             " _ " +
             " _|" +
             "|_ ";
-    private static final String THREE_CELL =
+    public static final String THREE_CELL =
             " _ " +
             " _|" +
             " _|";
-    private static final String FOUR_CELL =
+    public static final String FOUR_CELL =
             "   " +
             "|_|" +
             "  |";
-    private static final String FIVE_CELL =
+    public static final String FIVE_CELL =
             " _ " +
             "|_ " +
             " _|";
-    private static final String SIX_CELL =
+    public static final String SIX_CELL =
             " _ " +
             "|_ " +
             "|_|";
-    private static final String SEVEN_CELL =
+    public static final String SEVEN_CELL =
             " _ " +
             "  |" +
             "  |";
-    private static final String EIGHT_CELL =
+    public static final String EIGHT_CELL =
             " _ " +
             "|_|" +
             "|_|";
-    private static final String NINE_CELL =
+    public static final String NINE_CELL =
             " _ " +
             "|_|" +
             " _|";
-
-    private char digitAsChar;
-    private char digitAsString;
-
 }
