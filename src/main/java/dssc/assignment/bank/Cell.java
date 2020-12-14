4367ba0ba76b25e5f3d9c471755d2889aa1a8265
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2020-12-14 19:17:30 +0100

    Update Digit class

--- src/main/java/dssc/assignment/bank/Cell.java
+++ src/main/java/dssc/assignment/bank/Cell.java
@@ -24,5 +24,40 @@ class Digit {
             " _ " +
             "| |" +
             "|_|";
-
+    private static final String ONE_CELL =
+            "  |" +
+            "  |" +
+            "  |";
+    private static final String TWO_CELL =
+            " _ " +
+            " _|" +
+            "|_ ";
+    private static final String THREE_CELL =
+            " _ " +
+            " _|" +
+            " _|";
+    private static final String FOUR_CELL =
+            "   " +
+            "|_|" +
+            "  |";
+    private static final String FIVE_CELL =
+            " _ " +
+            "|_ " +
+            " _|";
+    private static final String SIX_CELL =
+            " _ " +
+            "|_ " +
+            "|_|";
+    private static final String SEVEN_CELL =
+            " _ " +
+            "  |" +
+            "  |";
+    private static final String EIGHT_CELL =
+            " _ " +
+            "|_|" +
+            "|_|";
+    private static final String NINE_CELL =
+            " _ " +
+            "|_|" +
+            "  |";
 }
